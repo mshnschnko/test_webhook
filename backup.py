1eb51d6ba96616543d234f2a71db9343f0f20e09
mshnschnko <misha321654987i@gmail.com>
2023-04-24 17:45:09 +0300

    add schedule

--- backup.py
+++ backup.py
@@ -2,6 +2,9 @@ from dotenv import load_dotenv
 import os
 import platform
 import yadisk
+import asyncio
+import aioschedule
+import time
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
@@ -16,38 +19,46 @@ LOCAL_STORAGE = "storage"
 LOCAL_BACKUP_FOLDER = "storage/backup/"
 LOCAL_TEMP_FOLDER = "storage/temp/"
 
+async def backup():
+    try:
+        if os.path.isdir(f'{LOCAL_BACKUP_FOLDER}'):
+                os.system(f'rm -rf {LOCAL_BACKUP_FOLDER}*')
+        # y.download("/tg_storage/", "./storage/backup/")
+        for i in list(y.listdir(USERS_STORAGE_FOLDER)):
+            dir_name = i['name']
+            try:
+                os.mkdir(f'{LOCAL_BACKUP_FOLDER}{dir_name}')
+            except Exception as ex:
+                print(ex)
+            for j in list(y.listdir(f"{USERS_STORAGE_FOLDER}{dir_name}/")):
+                file_name = j['name']
+                y.download(f'{USERS_STORAGE_FOLDER}{dir_name}/{file_name}', f'{LOCAL_BACKUP_FOLDER}{dir_name}/{file_name}')
+        print(platform.system())
+        ext = str()
+        if platform.system() == 'Windows':
+            os.system(f'powershell Compress-Archive -Force "{os.path.join(LOCAL_STORAGE, "backup")}"\
+                        {os.path.join(LOCAL_STORAGE, "temp", "backup.zip")}')
+            # os.system(f'powershell Compress-Archive -Force "{os.path.join(LOCAL_STORAGE, "dump.sql")}"\
+            #             -Update {os.path.join(LOCAL_STORAGE, "temp", "backup.zip")}')
+            ext = 'zip'
+        elif platform.system() == 'Linux':
+            # os.system(f'zip -r {os.path.join(LOCAL_STORAGE, "temp", "backup.zip")} {os.path.join(LOCAL_STORAGE, "backup")}')
+            os.system(f'tar -jcvf {os.path.join(LOCAL_STORAGE, "temp", "backup.tar")}\
+                        {os.path.join(LOCAL_STORAGE, "backup")} {os.path.join(LOCAL_STORAGE, "dump.sql")}')
+            # os.system(f'tar -jrvf {os.path.join(LOCAL_STORAGE, "temp", "backup.tar")} {os.path.join(LOCAL_STORAGE, "dump.sql")}')
+            ext = 'tar'
+        y.upload(f'{os.path.join(LOCAL_STORAGE, "temp", f"backup.{ext}")}', f'{REMOTE_BACKUP_FOLDER}backup.{ext}', overwrite=True)
+        if os.path.isfile(os.path.join(LOCAL_STORAGE, "temp", f"backup.{ext}")):
+                    os.remove(os.path.join(LOCAL_STORAGE, "temp", f"backup.{ext}"))
+        if os.path.isdir(f'{LOCAL_BACKUP_FOLDER}'):
+                os.system(f'rm -rf {LOCAL_BACKUP_FOLDER}*')
+    except Exception as ex:
+        print(ex)
 
-try:
-    if os.path.isdir(f'{LOCAL_BACKUP_FOLDER}'):
-            os.system(f'rm -rf {LOCAL_BACKUP_FOLDER}*')
-    # y.download("/tg_storage/", "./storage/backup/")
-    for i in list(y.listdir(USERS_STORAGE_FOLDER)):
-        dir_name = i['name']
-        try:
-            os.mkdir(f'{LOCAL_BACKUP_FOLDER}{dir_name}')
-        except Exception as ex:
-            print(ex)
-        for j in list(y.listdir(f"{USERS_STORAGE_FOLDER}{dir_name}/")):
-            file_name = j['name']
-            y.download(f'{USERS_STORAGE_FOLDER}{dir_name}/{file_name}', f'{LOCAL_BACKUP_FOLDER}{dir_name}/{file_name}')
-    print(platform.system())
-    ext = str()
-    if platform.system() == 'Windows':
-        os.system(f'powershell Compress-Archive -Force "{os.path.join(LOCAL_STORAGE, "backup")}"\
-                    {os.path.join(LOCAL_STORAGE, "temp", "backup.zip")}')
-        # os.system(f'powershell Compress-Archive -Force "{os.path.join(LOCAL_STORAGE, "dump.sql")}"\
-        #             -Update {os.path.join(LOCAL_STORAGE, "temp", "backup.zip")}')
-        ext = 'zip'
-    elif platform.system() == 'Linux':
-        # os.system(f'zip -r {os.path.join(LOCAL_STORAGE, "temp", "backup.zip")} {os.path.join(LOCAL_STORAGE, "backup")}')
-        os.system(f'tar -jcvf {os.path.join(LOCAL_STORAGE, "temp", "backup.tar")}\
-                    {os.path.join(LOCAL_STORAGE, "backup")} {os.path.join(LOCAL_STORAGE, "dump.sql")}')
-        # os.system(f'tar -jrvf {os.path.join(LOCAL_STORAGE, "temp", "backup.tar")} {os.path.join(LOCAL_STORAGE, "dump.sql")}')
-        ext = 'tar'
-    y.upload(f'{os.path.join(LOCAL_STORAGE, "temp", f"backup.{ext}")}', f'{REMOTE_BACKUP_FOLDER}backup.{ext}', overwrite=True)
-    if os.path.isfile(os.path.join(LOCAL_STORAGE, "temp", f"backup.{ext}")):
-                os.remove(os.path.join(LOCAL_STORAGE, "temp", f"backup.{ext}"))
-    if os.path.isdir(f'{LOCAL_BACKUP_FOLDER}'):
-            os.system(f'rm -rf {LOCAL_BACKUP_FOLDER}*')
-except Exception as ex:
-    print(ex)
+
+if __name__ == "__main__":
+    aioschedule.every(1).day.do(backup)
+    loop = asyncio.get_event_loop()
+    while True:
+        loop.run_until_complete(aioschedule.run_pending())
+        time.sleep(1)
